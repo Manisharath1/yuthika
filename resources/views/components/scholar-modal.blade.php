b8d28a51e490973cd78d122539774eba417a2e0e
Manisha Rath <rathmanisha889@gmail.com>
2024-12-16 17:46:57 +0530

    model-edit

--- resources/views/components/scholar-modal.blade.php
+++ resources/views/components/scholar-modal.blade.php
@@ -160,11 +160,11 @@
                                    class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500">
                         </div>
 
-                        <div>
+                        {{-- <div>
                             <label class="block text-sm font-medium text-gray-700">Document Link</label>
                             <input type="url" name="document_link"
                                    class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500">
-                        </div>
+                        </div> --}}
 
                         <div>
                             <label class="block text-sm font-medium text-gray-700">No. of Publications</label>
