6087c44bf9ffe2f9423ac8f926ea04e6fd5effcb
Manisha Rath <rathmanisha889@gmail.com>
2025-01-04 13:58:43 +0530

    rename

--- app/View/Components/ProjectAdd.php
+++ app/View/Components/ProjectAdd.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class ProjectEdit extends Component
+class ProjectAdd extends Component
 {
     /**
      * Create a new component instance.
